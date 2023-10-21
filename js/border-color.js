35b8c5c7b345236de907d3b8ff3f89dad9fc9819
The Hieu <thehieu0814@gmail.com>
2023-10-21 11:32:23 +0700

    html

--- js/border-color.js
+++
@@ -1,8 +0,0 @@
-
-var listItems = document.querySelectorAll(".categories-list_number--ck");
-
-for (var i = 0; i < listItems.length; i++) {
-    listItems[i].style.border = "2px solid transparent";
-    listItems[i].style.borderImage = 'url("https://sofavila.vn/wp-content/uploads/2023/01/mau-pastel.jpg") 30 round';
-}
-
